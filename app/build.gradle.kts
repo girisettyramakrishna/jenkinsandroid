1ef8b543ac148b783dd2fece5651cc2b351243c7
girisettyramakrishna <girisettyramakrishna4@gmail.com>
2025-06-03 18:00:28 +0530

    Update build.gradle.ktsrfd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,40 @@
-apply plugin: 'com.android.application'
+plugins {
+    id("com.android.application")
+    kotlin("android") version "1.9.0" // If you use Kotlin
+}
 
 android {
-    compileSdkVersion 33
-    defaultConfig {
-        applicationId "com.ramki.jenkins"
-        minSdkVersion 21
-        targetSdkVersion 33
-        versionCode 1
-        versionName "1.0"
+    compileSdk = 33
 
-        // Enable Java 8 features if needed
-        compileOptions {
-            sourceCompatibility JavaVersion.VERSION_1_8
-            targetCompatibility JavaVersion.VERSION_1_8
-        }
+    defaultConfig {
+        applicationId = "com.ramki.jenkins"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
 
 dependencies {
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
-
-    // ZXing barcode scanner core library
-    implementation 'com.google.zxing:core:3.4.1'
-
-    // ZXing Android integration library
-    implementation 'com.journeyapps:zxing-android-embedded:4.3.0'  // Alternative to manual integration, supports BarcodeFormat
-
-    // ZBar barcode scanner library for Symbol class
-    implementation 'me.dm7.barcodescanner:zbar:1.9'
-
-    // AndroidX libraries (update if you use androidx)
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
-
-    // Other dependencies you might have
-    // implementation 'com.google.android.material:material:1.8.0'
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.google.zxing:core:3.4.1")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation("me.dm7.barcodescanner:zbar:1.9")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
-
